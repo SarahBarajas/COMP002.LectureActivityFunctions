46d81050429ae85ddceffb6e072bc82fe637b5ac
Sarah Barajas <sarahbarajas91@yahoo.com>
2024-10-30 13:08:07 -0700

    log end of first function

--- script.js
+++ script.js
@@ -14,4 +14,8 @@ console.log(calculateCircumference(10));
 
 // Define the first function
 function firstFunction() {
-    console.log("First function start"); // Log the start of the first function
+    console.log("First function start"); // Log start of the first function
+
+    secondFunction(); // Call second function
+
+    console.log("First function end"); // Log end of the first function
